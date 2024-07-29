f1e998085d1cdfe33f6de3d23e0e481275084960
hmedo <hmedo@7medo>
2024-07-29 23:03:42 +0300

    100-main.js_23-03

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,25 @@
+export default function createIteratorObject(report) {
+  const employeeComplex = Object.values(report.allEmployees).map((element) => element.map((employee) => employee));
+  const employeeArray = [];
+
+  for (const employees of employeeComplex) {
+    for (const employee of employees) {
+      employeeArray.push(employee);
+    }
+  }
+
+  return {
+    data: employeeArray,
+    [Symbol.iterator]() {
+      let index = 0;
+      return {
+        next: () => {
+          if (index < this.data.length) {
+            return ({ value: this.data[index++], done: false });
+          }
+          return ({ value: undefined, done: true });
+        },
+      };
+    },
+  };
+}
