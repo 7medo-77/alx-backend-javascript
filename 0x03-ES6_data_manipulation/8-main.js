2e2a96bd63807ec78f6a164d47c717ca522f9e82
hmedo <hmedo@7medo>
2024-08-02 22:37:54 +0300

    8-main.js_22-37

--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -2,3 +2,7 @@ import cleanSet from './8-clean_set.js';
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+
+test('returns jovi-aparte-apetit when bon is passed to a new set containing bonjovi, bonaparte, and bonappetit', () => {
+  expect(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit']), 'bon')).toBe('jovi-aparte-appetit');
+});
