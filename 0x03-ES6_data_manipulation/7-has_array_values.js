2cdb1a6e66ac0aa279a63857ff8cff8419e554b4
hmedo <hmedo@7medo>
2024-08-02 19:58:54 +0300

    7-has_array_values.js_19-58

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
 export default function hasValuesFromArray(set, inputArray) {
-  set.has(...inputArray);
+  for (const num of inputArray) {
+    if (!set.has(num)) {
+      return false;
+    }
+  }
+  return true;
 }
