f43a804237c95415631c7f4657d55586d2701020
hmedo <hmedo@7medo>
2024-08-03 03:14:16 +0300

    9-main.js_03-14

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,15 +1,11 @@
-/**
- * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {Map<string, Number>}
- */
 export default function groceriesList() {
-  const values = [
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ];
-  return new Map(values);
+  const newMap = new Map();
+
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
+
+  return newMap;
 }
---
+++ 0x03-ES6_data_manipulation/9-main.js
@@ -0,0 +1,5 @@
+import groceriesList from './9-groceries_list.js';
+import groceriesListCopy from './9-groceries_list_copy.js';
+
+console.log(groceriesList());
+console.log(groceriesListCopy());
