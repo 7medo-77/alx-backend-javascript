f220714b2e9b957114a77450558bbea25e4a1111
hmedo <hmedo@7medo>
2024-08-03 16:30:34 +0300

    10-update_uniq_items.js_16-30

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(inputMap) {
   if (!(inputMap instanceof Map)) {
     throw new Error('Cannot Process');
+  } else {
+    inputMap.forEach((value, key) => {
+      if (value === 1) {
+        inputMap.set(key, 100);
+      }
+    });
   }
-
-  inputMap.forEach((value, key) => {
-    if (value === 1) {
-      inputMap.set(key, 100);
-    }
-  });
 }
