9104c0628a5b676ad9f7416523a5fe8abab987b5
hmedo <hmedo@7medo>
2024-09-18 18:57:25 +0300

    1-stdin.js_18-57

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (input) => {
+  const userInput = input.trim();
+  console.log(`Your name is: ${userInput}`);
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
