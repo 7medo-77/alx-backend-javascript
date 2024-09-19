8a424d6f9c1655af2933b85cca15c35b3eef0714
hmedo <hmedo@7medo>
2024-09-19 16:56:14 +0300

    2-read_file.js_16-56

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,35 @@
 const fs = require('node:fs');
 
 function countStudents(fileLocation) {
-  fs.readFile(fileLocation, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      const dataList = data.split('\n');
-      const newList = dataList.slice(1, dataList.length - 1);
-      const trackFreq = new Map();
+  try {
+    const data = fs.readFileSync(fileLocation, 'utf8');
+    const dataList = data.split('\n');
+    const newList = dataList.slice(1, dataList.length - 1);
+    const trackFreq = new Map();
 
-      for (const record of newList) {
-        const cellList = record.split(',');
-        const firstName = cellList[0];
-        const track = cellList[cellList.length - 1];
+    for (const record of newList) {
+      const cellList = record.split(',');
+      const firstName = cellList[0];
+      const track = cellList[cellList.length - 1];
 
-        if (cellList.length === 0) {
-          continue;
-        }
-
-        if (trackFreq.has(track)) {
-          trackFreq.get(track).push(firstName);
-        } else {
-          const firstNameArray = [];
-          firstNameArray.push(firstName);
-          trackFreq.set(track, firstNameArray);
-        }
+      if (cellList.length === 0) {
+        continue;
       }
-      console.log(`Number of students: ${newList.length}`);
-      for (const [key, value] of trackFreq) {
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+
+      if (trackFreq.has(track)) {
+        trackFreq.get(track).push(firstName);
+      } else {
+        const firstNameArray = [];
+        firstNameArray.push(firstName);
+        trackFreq.set(track, firstNameArray);
       }
     }
-  });
+    console.log(`Number of students: ${newList.length}`);
+    for (const [key, value] of trackFreq) {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
