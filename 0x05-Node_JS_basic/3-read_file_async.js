80f8ed6925c5ff6300bd3e72620872e4aa042b17
hmedo <hmedo@7medo>
2024-09-22 00:59:55 +0300

    3-read_file_async.js_00-59

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,33 +5,34 @@ function countStudents(fileLocation) {
     fs.readFile(fileLocation, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
-      }
-      const dataList = data.split('\n');
-      const newList = dataList.filter((row, index) => {
-        const recordLength = dataList[0].split(',').length;
-        return index !== 0 && row.split(',').length === recordLength;
-      });
+      } else {
+        const dataList = data.split('\n');
+        const newList = dataList.filter((row, index) => {
+          const recordLength = dataList[0].split(',').length;
+          return index !== 0 && row.split(',').length === recordLength;
+        });
 
-      const trackFreq = new Map();
+        const trackFreq = new Map();
 
-      for (const record of newList) {
-        const cellList = record.split(',');
-        const firstName = cellList[0];
-        const track = cellList[cellList.length - 1];
+        for (const record of newList) {
+          const cellList = record.split(',');
+          const firstName = cellList[0];
+          const track = cellList[cellList.length - 1];
 
-        if (trackFreq.has(track)) {
-          trackFreq.get(track).push(firstName);
-        } else {
-          const firstNameArray = [];
-          firstNameArray.push(firstName);
-          trackFreq.set(track, firstNameArray);
+          if (trackFreq.has(track)) {
+            trackFreq.get(track).push(firstName);
+          } else {
+            const firstNameArray = [];
+            firstNameArray.push(firstName);
+            trackFreq.set(track, firstNameArray);
+          }
         }
+        console.log(`Number of students: ${newList.length}`);
+        for (const [key, value] of trackFreq) {
+          console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+        }
+        resolve(true);
       }
-      console.log(`Number of students: ${newList.length}`);
-      for (const [key, value] of trackFreq) {
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-      }
-      resolve(true);
     });
   });
 }
